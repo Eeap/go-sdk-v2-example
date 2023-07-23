81e1481d696cd54d89f95af19a3184f27f720f72
Eeap <kdg97811@naver.com>
2023-07-23 13:20:11 +0900

    refactor: add pagingInstanceTypes

--- ec2/ec2.go
+++ ec2/ec2.go
@@ -8,26 +8,19 @@ import (
 	"log"
 )
 
+type EC2DataTypes struct {
+	instanceTypes []types.InstanceTypeInfo
+}
+
 func GetEC2InstanceTypes() []types.InstanceTypeInfo {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	svc := ec2.NewFromConfig(cfg)
-	filterType := "instance-type"
-	instanceName := "t1.micro"
-	resp, err := svc.DescribeInstanceTypes(context.TODO(), &ec2.DescribeInstanceTypesInput{
-		Filters: []types.Filter{
-			{
-				Name:   &filterType,
-				Values: []string{instanceName},
-			},
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	return resp.InstanceTypes
+	client := ec2.NewFromConfig(cfg)
+	ec2InstanceTypes := &EC2DataTypes{}
+	pagingInstanceTypes(client, nil, ec2InstanceTypes)
+	return ec2InstanceTypes.instanceTypes
 	//for _, object := range resp.InstanceTypes {
 	//	objectData := make(map[string]any)
 	//	objectData["InstanceType"] = string(object.InstanceType)
@@ -42,4 +35,25 @@ func GetEC2InstanceTypes() []types.InstanceTypeInfo {
 	//	fmt.Println(string(result))
 	//}
 	//return nil
+}
+
+func pagingInstanceTypes(client *ec2.Client, nextToken *string, ec2InstanceTypes *EC2DataTypes) {
+	//filterType := "instance-type"
+	//instanceName := "t1.micro"
+	resp, err := client.DescribeInstanceTypes(context.TODO(), &ec2.DescribeInstanceTypesInput{
+		NextToken: nextToken,
+		//Filters: []types.Filter{
+		//	{
+		//		Name:   &filterType,
+		//		Values: []string{instanceName},
+		//	},
+		//},
+	})
+	if err != nil {
+		return
+	}
+	if resp.NextToken != nil {
+		pagingInstanceTypes(client, resp.NextToken, ec2InstanceTypes)
+	}
+	ec2InstanceTypes.instanceTypes = append(ec2InstanceTypes.instanceTypes, resp.InstanceTypes...)
 }
